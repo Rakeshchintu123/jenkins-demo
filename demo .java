9cc81b29646eca08ff73e947700f930f7ffc86be
Rakesh kumar k <rakeshvijay058@gmail.com>
2026-05-30 15:59:55 +0530

    Convert HTML to Java program with hello message

--- demo .java
+++ demo .java
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>first project</h1>
-    <h2>tech</h2>
-    <h3>i m vijay</h3>
-</body>
-</html>
+public class demo{
+    public static void main(String args []){
+        System.out.println("hello vijay");
+    }
+}
