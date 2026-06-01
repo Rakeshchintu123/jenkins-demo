8b30b1d00c569c9dd8426fc3aef5c108edc1d2fd
Rakesh kumar k <rakeshvijay058@gmail.com>
2026-06-01 22:26:45 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,6 @@
 public class demo{
     public static void main(String args []){
-        System.out.println("hello vijay");
+        System.out.println("hello vijay")
+            echo "New Change" ;
     }
 }
