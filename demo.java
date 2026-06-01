61806779610ca622e7cd703d74c9e861d3bc811d
Rakesh kumar k <rakeshvijay058@gmail.com>
2026-06-01 22:28:47 +0530

    Replace echo statement with System.out.println

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo{
     public static void main(String args []){
         System.out.println("hello vijay")
-            echo "New Change" ;
+            system.out.println("hello rakesh") ;
     }
 }
